7b5927dea4ef28bef2bbc8b3e349586953dd5f8e
naoto hamada <naoto.hamada@findy.co.jp>
2022-07-11 00:28:45 +0900

    Update rails 6.1 (#229)
    * Update rails 6.1
    
    * Update rails 6.1

--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,14 @@
-require_relative 'boot'
+require_relative "boot"
 
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
 # require "active_job/railtie"
 require "active_record/railtie"
-#require "active_storage/engine"
+# require "active_storage/engine"
 require "action_controller/railtie"
 # require "action_mailer/railtie"
-#require "action_mailbox/engine"
+# require "action_mailbox/engine"
 # require "action_text/engine"
 # require "action_view/railtie"
 # require "action_cable/engine"
@@ -22,15 +22,19 @@ Bundler.require(*Rails.groups)
 module App
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 6.1
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+
+    # Only loads a smaller set of middleware suitable for API only apps.
+    # Middleware like session, flash, cookies can be added back manually.
+    # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Don't generate system test files.
-    config.generators.system_tests = nil
   end
 end
